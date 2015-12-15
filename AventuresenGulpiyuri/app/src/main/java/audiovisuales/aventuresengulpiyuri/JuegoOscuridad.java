4082ffb3a57c337937f8c3f17fdd94c6315160f5
vegaandre <andrea.gjn@gmail.com>
2015-12-15 11:56:46 +0100

    generado clases juego oscuridad

---
+++ AventuresenGulpiyuri/app/src/main/java/audiovisuales/aventuresengulpiyuri/JuegoOscuridad.java
@@ -0,0 +1,18 @@
+package audiovisuales.aventuresengulpiyuri;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andrea on 15/12/2015.
+ */
+public class JuegoOscuridad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego_oscuridad);
+    }
+
+    
+}
