84a464788fa4039558e644909fbaa7ef0a3ced6d
meri294 <merigg94@gmail.com>
2016-01-09 18:28:17 +0100

    TTS fin

--- AventuresenGulpiyuri/app/src/main/java/audiovisuales/aventuresengulpiyuri/Fin.java
+++ AventuresenGulpiyuri/app/src/main/java/audiovisuales/aventuresengulpiyuri/Fin.java
@@ -1,11 +1,18 @@
 package audiovisuales.aventuresengulpiyuri;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import util.Configuracion;
+import util.Utilidades;
+
 public class Fin extends ActionBarActivity {
+
+    private TextToSpeech tts;
+    private Configuracion conf = Configuracion.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,20 @@ public class Fin extends ActionBarActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_fin);
+        if (conf.getLecturaAutomatica()){
+            if (!Utilidades.verificaConexion(this)) {
+                if (conf.getRepetirAviso())
+                    Utilidades.mostrarVentanaErrorDeConexion(this);
+            }
+            else {
+                tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        tts.setSpeechRate(Float.valueOf("0.95"));
+                        tts.speak(getResources().getString(R.string.fin), TextToSpeech.QUEUE_ADD, null);
+                    }
+                });
+            }
+        }
     }
 }
